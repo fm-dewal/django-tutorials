9e4567b6fa9d5abc65a454cf5e3d117d83aae11e
Faras Mohan Dewal <faras.dewal@gmail.com>
2023-08-25 18:58:42 +0530

    Add admin, add views:url:F()
    Signed-off-by: Faras Mohan Dewal <faras.dewal@gmail.com>

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
-    path(route="", view=views.index, name="index")
+    # eg: /polls/
+    path(route = "", view = views.index, name = "index"),
+    # eg: /polls/2/
+    path(route = "<int:question_id>", view = views.detail, name = "detail"),
+    # eg: /polls/2/results
+    path(route = "<int:question_id>/results", view = views.results, name = "results"),
+    # eg: /polls/2/voting
+    path(route = "<int:ques_id>/vote", view = views.vote, name = "vote"),
 ]
